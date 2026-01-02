9110a385e44082932c1c477951a80dbb56c8f7ed
Enzzo Cavallo <souenzzo@gmail.com>
2026-01-02 10:49:13 -0300

    NBlockingSSL working with SNI

--- test-servers/nblocking-ssl.sh
+++ test-servers/nblocking-ssl.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env sh
 
+
+
 (
   cd ..
+  lein javac
   ## Works
   PORT=8443
+  java \
+      -Djavax.net.ssl.trustStore=test-servers/ssl/truststore.jks \
+      -Djavax.net.ssl.trustStorePassword=httpkit \
+      -Djavax.net.ssl.trustStoreType=JKS \
+      -Dorg.httpkit.client.NBlockingSSL.host=api.localhost \
+      "-Dorg.httpkit.client.NBlockingSSL.port=$PORT" \
+      -Dorg.httpkit.client.NBlockingSSL.file=project.clj \
+      -classpath "$(lein with-profile +dev classpath)" \
+      org.httpkit.client.NBlockingSSL
   ## Fails
-  # PORT=9443
-  lein javac
+  PORT=9443
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
   java \
     -Djavax.net.ssl.trustStore=test-servers/ssl/truststore.jks \
     -Djavax.net.ssl.trustStorePassword=httpkit \
@@ -16,4 +28,5 @@
     -Dorg.httpkit.client.NBlockingSSL.file=project.clj \
     -classpath "$(lein with-profile +dev classpath)" \
     org.httpkit.client.NBlockingSSL
+
 )
