25f698dec06be847ff48c3e8d098d6d626a0bf2e
Enzzo Cavallo <souenzzo@gmail.com>
2026-01-06 13:50:09 -0300

    Reproduce OK!

--- test-servers/httpclient-test.sh
+++ test-servers/httpclient-test.sh
@@ -7,6 +7,4 @@ clojure \
   -Sdeps '{:deps {http-kit/http-kit {:mvn/version "2.8.1"}}}' -M \
   -e "(require '[org.httpkit.client :as c])" \
   -e '(def rs ["https://api.localhost:8443" "https://api.localhost:9443"])' \
-  -e '(doseq [r rs] (-> r c/get deref :status prn))'
-
-
+  -e '(doseq [r rs] (-> {:insecure? true :method :get :url r} c/request deref ((juxt :status :error)) prn))'
